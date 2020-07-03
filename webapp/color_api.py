f4b43da4b0a594f7c6bbfa2f74614dd42cb1b469
Aaron Watkins <ac.watkins80@gmail.com>
2020-07-03 13:49:33 -0400

    rename main file add logging

---
+++ webapp/color_api.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import flask
+from flask import request, jsonify
+
+
+'''   ############ USAGE ###############
+logger.info("info message")
+logger.warning("warning message")
+logger.error("error message")
+'''
+logger = logging.getLogger(__name__)   
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+# create console handler and set level to info
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s [color_api.py] [%(levelname)-5.5s]  %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+@app.route('/pilight/<color>', methods=['GET'])
+def api_all(color):
+    print(color) 
+    return color 
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+app.run(host='0.0.0.0', port=12345)
