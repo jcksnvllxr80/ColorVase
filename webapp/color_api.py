ad784a1c2a0263d3ef9442d944c24d274f70ff18
Aaron Watkins <ac.watkins80@gmail.com>
2020-07-03 14:17:36 -0400

    change strings

--- webapp/color_api.py
+++ webapp/color_api.py
@@ -26,20 +26,20 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     logger.info("ColorApi: Change the color of a string of ws2811 LEDs on a raspi with google assistant.") 
-    return '''<h1>ColorApi</h1>
-<p>Change the color of a string of ws2811 LEDs on a raspi with google assistant.</p>'''
+    return '''<h1>ColorApi</h1><p>Change the color of a string of ws2811 LEDs on a raspi with google assistant.</p>''' + "\n"
 
 
 @app.route('/pilight/<color>', methods=['GET'])
 def api_all(color):
-    logger.info("The keyword used in the command was \"" + str(color) + "\"") 
-    return color 
+    logger.info("A request was made using the color API. The keyword used in the request was \"" + str(color) + "\"") 
+    return '''<h1>ColorApi</h1><p>A request was made using the color API. The keyword used in the request was \"''' +
+        color + '''\"</p>''' + "\n"
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     logger.error("404: The resource could not be found.") 
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    return "<h1>404</h1><p>The resource could not be found.</p>\n", 404
 
 
 app.run(host='0.0.0.0', port=12345)
