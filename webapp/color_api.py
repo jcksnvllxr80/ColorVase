ee9e3787b60342264623bc5c67d1e7036cc0580c
Aaron Watkins <ac.watkins80@gmail.com>
2020-07-04 02:35:37 -0400

    dont reload

--- webapp/color_api.py
+++ webapp/color_api.py
@@ -437,4 +437,4 @@ if __name__ == '__main__':
         port = config_port
     else:
         port = 5000
-    app.run(host='0.0.0.0', port=port) #, use_reloader=False)
+    app.run(host='0.0.0.0', port=port, use_reloader=False)
